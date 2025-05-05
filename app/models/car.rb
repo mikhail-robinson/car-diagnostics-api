a3a19e00e0d0b38e08508e92caf3e8dbb4b5dff7
Mikhail Robinson <126931597+mikhail-robinson@users.noreply.github.com>
2025-05-05 15:43:03 +1200

    fix incorrect one to many relationship

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,4 @@
 class Car < ApplicationRecord
-  has_many :service_entries, dependent: :destroy
+  has_many :service_history_entries, dependent: :destroy
   validates :plate, :make, :model, :year, presence: true
 end
