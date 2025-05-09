c7f782de757a57cc15a7ebda02cff85656883413
Mikhail Robinson <126931597+mikhail-robinson@users.noreply.github.com>
2025-05-10 10:30:33 +1200

    update cors for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://localhost:3000", "http://localhost:5173", "https://*.vercel.app"
 
     resource "*",
       headers: :any,
-      methods: [ :get, :post, :options ]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
